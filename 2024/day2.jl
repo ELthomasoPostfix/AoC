5b0d8d615a0baaabcda10e6348ab714a9659352c
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-12-06 23:55:34 +0100

    Solve 2024 day2.
    This solution is not nice.
    Rework it later.

--- 2024/day2.jl
+++ 2024/day2.jl
@@ -27,7 +27,7 @@ function part1()
         gte0 = (val) -> val >= 0
         increasing = all(gte0, diffs)
         
-        lt0 = (val) -> val < 0
+        lt0 = (val) -> val <= 0
         decreasing = all(lt0, diffs)
 
         safe1 = increasing | decreasing
@@ -48,21 +48,36 @@ function find_error_locations(datavec::Array)
     # (1) The levels are either all increasing or all decreasing.
     diffs = datavec[2:end] - datavec[1:end-1]
 
+    # For an incr (decr) series, all decr (incr) are errors.
     gte0 = (val) -> val >= 0
-    increasing = gte0.(diffs)
-    
-    lt0 = (val) -> val < 0
-    decreasing = lt0.(diffs)
+    increasing = @. !gte0(diffs)
+    lt0 = (val) -> val <= 0
+    decreasing = @. !lt0(diffs)
 
-    bools1 = @. !(increasing | decreasing)
+    # If the series is monotonically incr (decr), then default to no errors.
+    # FIXME: Commenting these two lines solves everything???
+    increasing = !all(increasing) .& increasing
+    decreasing = !all(decreasing) .& decreasing
+
+    # If the series is monotonically increasing, then default to no errors.
+    increasing = vcat(increasing[end], increasing[1:(end-1)])
+    increasing = vcat(decreasing[end], decreasing[1:(end-1)])
 
     # (2) Any two adjacent levels differ by at least one and at most three.
     diffbtwn = (val) -> 1 <= abs(val) & abs(val) <= 3
-    bools2 = diffbtwn.(diffs)
-    bools2 = @. !bools2
+    bools_diff = diffbtwn.(diffs)
+    bools_diff = @. !bools_diff
+
+    # Both incr and decr series must conform to the abs diff size constraints.
+    bools_incr = @. increasing | bools_diff
+    bools_decr = @. decreasing | bools_diff
+
 
     # The error indexes can be used to determine safety.
-    return findall(e -> e, bools1 .| bools2)
+    indexes = findall(e -> e, bools_incr .| bools_decr)
+    # Each erroneous diff results in two error indexes
+    indexes = union(indexes, indexes .+ 1)
+    return indexes
 end
 
 function part2()
@@ -70,25 +85,30 @@ function part2()
 
     # A sequence is ascending iff. all differences are >= 0.
     safe = Array{Bool}(undef, length(data))
-    for (idx, report) in enumerate(data)
-        datavec = data[idx]
+    for (idx1, report) in enumerate(data)
+        datavec = report
 
         error_idexes = find_error_locations(datavec)
         issafe = length(error_idexes) == 0
 
+        printable = copy(report)
+        printable[error_idexes] = fill(999, length(error_idexes))
+        println(printable)
         for idx in error_idexes
-            datavec = datavec[.!in.(1:end,([idx],))]
+            datavec = report[.!in.(1:end,([idx],))]
             error_indexes_corrected = find_error_locations(datavec)
             issafe = length(error_indexes_corrected) == 0
             if issafe
+                println("  $datavec")
                 break
             end
         end
 
         # Store
-        safe[idx] = issafe
+        safe[idx1] = issafe
     end
 
+    println(safe)
     result = sum(safe)
     return result
 end
