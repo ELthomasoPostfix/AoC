d232ebdd7e1e8dc45f2771961206c9bb765f3a03
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-12-07 15:41:24 +0100

    Solve 2024 day3 part 1 & 2.

---
+++ 2024/day3.jl
@@ -0,0 +1,47 @@
+
+"""Parse the input into a form that both parts can use.
+
+    @param[in] The path to the input file.
+"""
+function parse_input(file_path::String)
+    return string(readlines(file_path)...)
+end
+
+function part1()
+    data = parse_input("./data3.txt")
+
+    # Sum each "mul(X,Y)" match
+    regex = r"mul\(([0-9]+),([0-9]+)\)"
+    total::Int64 = 0    # Require Int64 to prevent overflow!
+    for m in eachmatch(regex, data)
+        larg, rarg = m.captures
+        total += parse(Int64, larg) * parse(Int64, rarg)
+    end
+
+    result = total
+    return result
+end
+
+function part2()
+    data = parse_input("./data3.txt")
+
+    # Replace each subsequence "don't() ... do()" OR
+    # "don't() ... EOF" by the empty string.
+    # Then only "do()" copies of "mul(X,Y)" remain.
+    regex = r"don't\(\).+?((do\(\))|$)"
+    data = replace(data, regex => s"")
+
+    # Sum each "mul(X,Y)" match
+    regex = r"mul\(([0-9]+),([0-9]+)\)"
+    total::Int64 = 0    # Require Int64 to prevent overflow!
+    for m in eachmatch(regex, data)
+        larg, rarg = m.captures
+        total += parse(Int64, larg) * parse(Int64, rarg)
+    end
+
+    result = total
+    return result
+end
+
+println(part1())
+println(part2())
