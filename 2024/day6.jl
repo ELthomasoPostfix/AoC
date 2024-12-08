3b2a8dd65d47c3f77ba532c610ff6db083972ad6
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-12-08 14:02:12 +0100

    Generalize day6 part 1 code.

--- 2024/day6.jl
+++ 2024/day6.jl
@@ -29,53 +29,69 @@ function parse_input(file_path::String)
     return mat, guard
 end
 
+"""Compute the results of performing one straight line walk until a wall.
+
+    @return (
+        The new guard (x, y) coords,
+        The new heading (cardinal direction) ID,
+        Whether 
+    )
+"""
+function walk(grid, guard, heading::Int16)
+    #=  heading  |  look dir  |  str dir
+            0     |     UP     |   PREV
+            1     |    RIGHT   |   NEXT
+            2     |    DOWN    |   NEXT
+            3     |    LEFT    |   PREV
+    =#
+    x, y = guard
+    h, w = size(grid)
+    vertical::Bool = (heading % 2) == 0
+
+    # A method to find the nearest wall in the path of the guard.
+    method = heading%3 == 0 ? findprev : findnext
+    # The column or row on which the guard will now move.
+    path::String = vertical ? string(grid[:,x]...) : string(grid[y,:]...)
+    # The guard's starting position on the path.
+    start = vertical ? y : x
+
+    # Walk until you hit a wall, and then immediately turn.
+    # This essentially reduces the two rules to one rule.
+    z = method(==(CHR_WALL), path, start)
+
+    left_grid::Bool = false
+    if z === nothing
+        guard = vertical ?
+            (x, max(1, h * (heading - 1))) :
+            (max(1, w * -(heading - 2)), y)
+        left_grid = true
+    else
+        z += sign(start - z) # z is on top of the wall, move one step back
+        guard = vertical ? (x, z) : (z, y)
+        heading = (heading + 1) % 4
+    end
+
+    return guard, heading, left_grid
+end
+
 function part1()
     data = parse_input("./data6.txt")
 
     grid, guard = data
-    h, w = size(grid)
 
     heading::Int16 = 0
-    while guard !== nothing
-        #=  heading  |  look dir  |  str dir
-               0     |     UP     |   PREV
-               1     |    RIGHT   |   NEXT
-               2     |    DOWN    |   NEXT
-               3     |    LEFT    |   PREV
-        =#
+    done::Bool = false
+    while !done
+        target, heading, done = walk(grid, guard, heading)
+
         x, y = guard
-        vertical::Bool = (heading % 2) == 0
-
-        # A method to find the nearest wall in the path of the guard.
-        method = heading%3 == 0 ? findprev : findnext
-        # The column or row on which the guard will now move.
-        path::String = vertical ? string(grid[:,x]...) : string(grid[y,:]...)
-        # The guard's starting position on the path.
-        start = vertical ? y : x
-
-        
-        # Walk until you hit a wall, and then immediately turn.
-        # This essentially reduces the two rules to one rule.
-        z = method(==(CHR_WALL), path, start)
-        
-        if z === nothing
-            target = vertical ?
-                (x, max(1, h * (heading - 1))) :
-                (max(1, w * -(heading - 2)), y)
-
-            guard = nothing
-        else
-            z += sign(start - z) # z is on top of the wall, move one step back
-            target = vertical ? (x, z) : (z, y)
-            guard = target
-            heading = (heading + 1) % 4
-        end
         u, v = target
         row_ran = min(u,x):max(u,x)
         col_ran = min(v,y):max(v,y)
         grid[col_ran, row_ran] .= CHR_SEEN
-    end
 
+        guard = target
+    end
 
     result = count(==(CHR_SEEN), grid)
     return result
