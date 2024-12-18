e2fdbd3efbb025d58b6a0dbb8019b0b3a727a593
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-12-18 23:59:02 +0100

    Solve 2024 day17 part 2.
    Throw away the old part 1 solution.
    For part 2, it was easier to simply
     decipher the program written in the
     input, and then implement it as
     an actual program as part of debugging.

--- 2024/day17.jl
+++ 2024/day17.jl
@@ -6,72 +6,105 @@
 function parse_input(file_path::String)
     lines::Array{String} = readlines(file_path)
 
-    OP_COMBO[OP_RA] = parse(Int64, lines[1][13:end])
-    OP_COMBO[OP_RB] = parse(Int64, lines[2][13:end])
-    OP_COMBO[OP_RC] = parse(Int64, lines[3][13:end])
+    A::Int64 = parse(Int64, lines[1][13:end])
+    B::Int64 = parse(Int64, lines[2][13:end])
+    C::Int64 = parse(Int64, lines[3][13:end])
+    instructions::Vector{Int64} =
+        parse.(Int64, (split(lines[5][10:end], ',')))
 
-    return parse.(Int64, (split(lines[5][10:end], ',')))
+    return A, B, C, instructions
 end
 
-# Use a Dict to circumvent 1-indexed arrays.
-OP_RA::Int64 = 4
-OP_RB::Int64 = 5
-OP_RC::Int64 = 6
-OP_COMBO = Dict(
-    0 => 0, 1 => 1, 2 => 2, 3 => 3, # Literals
-    OP_RA => 0, OP_RB => 0, OP_RC => 0, # Registers
-    7 => nothing # Invalid opcode
-)
+"""An implementation of my specific input assembly.
 
-INSTRUCTIONS = Dict(
-    0 => ((v, pc) -> OP_COMBO[OP_RA] = div(OP_COMBO[OP_RA], 2^OP_COMBO[v])),
-    1 => ((v, pc) -> OP_COMBO[OP_RB] = xor(v, OP_COMBO[OP_RB])),
-    2 => ((v, pc) -> OP_COMBO[OP_RB] = OP_COMBO[v] % 8),
-    # Do -2 to the program counter, to counteract the fixed incrementing of
-    # the program counter?
-    3 => ((v, pc) -> (OP_COMBO[OP_RA] != 0) ? (pc[] = v+1) : nothing),
-    4 => ((v, pc) -> OP_COMBO[OP_RB] = xor(OP_COMBO[OP_RB], OP_COMBO[OP_RC])),
-    5 => ((v, pc) -> OP_COMBO[v] % 8),
-    6 => ((v, pc) -> OP_COMBO[OP_RB] = div(OP_COMBO[OP_RA], 2^OP_COMBO[v])),
-    7 => ((v, pc) -> OP_COMBO[OP_RC] = div(OP_COMBO[OP_RA], 2^OP_COMBO[v])),
-)
+    Program: 2,4,1,1,7,5,1,5,0,3,4,3,5,5,3,0
+
+
+    0     2,4    bst 4    B=A%8
+    2     1,1    bxl 1    B=B xor 1
+    4     7,5    cdv 5    C=A//2^B
+    8     1,5    bxl 5    B=B xor 5
+    10    0,3    adv 3    A=A//2^3
+    12    4,3    bxc 3    B=B xor C
+    14    5,5    out 5    out B
+    16    3,0    jnz 0    jnz A, 0
+
+    A := INPUT
+    B := 0
+    C := 0
+    while A != 0
+        B = (A%8) xor 1
+        C = A // B
+        B = (B xor 5) xor C
+        out B
+        A = A // 8
+"""
+function solve(A, B, C)
+    outlst = []
+    while A != 0
+        B = xor(A%8, 1)
+        C = div(A, 2^B)
+        B = xor(xor(B, 5), C)
+        push!(outlst, B%8)
+        A = div(A, 8)
+    end
+    return join(map(string, outlst), ',')
+end
+
+"""All computations for a single iteration's out value."""
+function digit(a)
+    return xor(xor(a%8, 1, 5), div(a, 2^(xor(a%8, 1)))) % 8
+end
+
+"""Recursively find the 'A' that duplicates the instructions/program."""
+function evlos(instructions; A::Int64=0)
+    # Base case: No more instructions to predict A for.
+    if isempty(instructions)
+        return A
+    end
+
+    #= Each iteration of the algorithm drops the division remainder:
+            Anew <- A // 8
+      To discover the original A, try all possible remainders: rem in 0..7.
+            A = Anew * 8 + rem
+    =#
+    viable_sols = collect(A*8 .+ (0:7))
+    # All viable sols that actually result in outputting the expected digit are
+    # valid solutions.
+    valid_sols = findall(==(instructions[end]), digit.(viable_sols))
+
+    # The interùediate 'A' built based on the viable solution choices of
+    # the callers is not consistent for for this digit. Notify the caller.
+    if isempty(valid_sols)
+        return -1
+    end
+
+    # Check if the newly computed 'A' is consistent with all remaining digits.
+    for match in valid_sols
+        result = evlos(instructions[1:end-1], A=viable_sols[match])
+        # If this 'A' was consistent, then pass up the recursively computed 'A'.
+        if result != -1
+            return result
+        end
+    end
+
+    # None of the valid solutions were consistent.
+    return -1
+end
 
 function part1()
     data = parse_input("./data17.txt")
 
-    # Program counter, but the Julia 1-indexed version
-    pc::Ref{Int64} = Ref(1)
-    outlst::Array{String} = []
-    while 1 <= pc[] <= length(data)
-        # Load instruction
-        opcode  = data[pc[]]
-        operand = data[pc[]+1]
-
-        @assert operand != 7 "Combo operand 7 is an invalid operand!"
-
-        out = INSTRUCTIONS[opcode](operand, pc)
-
-        if opcode == 3 && out !== nothing
-            continue
-        end
-
-        if opcode == 5
-            push!(outlst, string(out))
-        end
-
-        # Always move program counter
-        pc[] = pc[] + 2
-    end
-
-    return join(outlst, ',')
+    A,B,C, = data
+    return solve(A,B,C)
 end
 
 function part2()
     data = parse_input("./data17.txt")
 
-    result = data
-    return result
+    _,_,_,instructions, = data
+    return evlos(instructions)
 end
 
 println(part1())
-# println(part2())
+println(part2())
