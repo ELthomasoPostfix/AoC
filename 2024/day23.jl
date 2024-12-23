4f910861a64288762c22b01f4c885878d6b3d060
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-12-23 13:47:29 +0100

    Add comments to 2024 day23.
    I now realize that this entire day is
     just finding k-cliques.

--- 2024/day23.jl
+++ 2024/day23.jl
@@ -20,84 +20,24 @@ function parse_input(file_path::String)
     return connections
 end
 
-function dot(g)
-    of = open("out.txt", "w")
-    write(of, "digraph {\n")
-    for (src, dsts) in pairs(g)
-        for dst in dsts
-            write(of, "  $src -> $dst\n")
-        end
-    end
-    write(of, "}\n")
-end
-
-"""Tarjan's algorithm for SCCs.
-
-    Pseudocode taken from https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
-"""
-function tarjan(graph::Dict)
-    stack = Stack{String}()
-    metadata::Dict = Dict{String, Tuple{Int64, Int64, Bool}}()
-    sccs::Vector{Vector{String}} = []
-    index::Int64 = 0
-    for v in keys(graph)
-        if !(v in keys(metadata))
-            sub_sccs = strongconnect(v, index, graph, stack, metadata)
-            sccs = vcat(sccs, sub_sccs)
-        end
-    end
-    return sccs
-end
-
-function strongconnect(vertex::S, index::Int64, graph::Dict, stack::Stack, metadata::Dict) where S <: String
-    sccs = []
-    metadata[vertex] = (index, index, true)
-    index += 1
-    push!(stack, vertex)
-
-    isonstack(v) = metadata[v][3]
-
-    for successor in graph[vertex]
-        if !(successor in keys(metadata))
-            sub_sccs = strongconnect(successor, index, graph, stack, metadata)
-            sccs = vcat(sccs, sub_sccs)
-            _, slidx, = metadata[successor]
-            vidx, vlidx, vonstack = metadata[vertex]
-            metadata[vertex] = (vidx, min(vlidx, slidx), vonstack)
-        elseif isonstack(successor)
-            sidx, = metadata[successor]
-            vidx, vlidx, vonstack = metadata[vertex]
-            metadata[vertex] = (vidx, min(vlidx, sidx), vonstack)
-        end
-    end
-
-    vidx, vlidx, vonstack = metadata[vertex]
-    if vlidx == vidx
-        scc = []
-        successor = ""
-        while successor != vertex
-            successor = pop!(stack)
-            sidx, slidx, = metadata[successor]
-            metadata[successor] = (sidx, slidx, false)
-            push!(scc, successor)
-        end
-        push!(sccs, scc)
-        return sccs
-    end
-    return []
-end
-
 function part1()
     data = parse_input("./data23.txt")
 
-    triangles = Set()
+    triangles::Set = Set()
     for p1 in keys(data)
+        # We only care about triangles containing a name starting in 't'.
+        # We iterate over all vertices, so enforce this for the initial vertex.
         if p1[1] != 't'
             continue
         end
+        # For all states reachable in one (1) transition.
         for p2 in data[p1]
+            # For all states reachable in two (2) transitions.
             for p3 in data[p2]
+                # Add only the triangles: p1 -> p2 -> p3 -> p1
                 if p1 in data[p3]
+                    # Sort so that the set of triangles retains only one
+                    # copy of each triangle.
                     push!(triangles, sort([p1, p2, p3]))
                 end
             end
@@ -112,7 +52,14 @@ function part2()
 
     groups = []
     for p1 in keys(data)
+        # Note: WE DONT CARE ABOUT 't' BEING IN A NAME ANYMORE.
+        # We just want to find the largest LAN party in general.
         group = Set([p1])
+        # A LAN party is a k-clique in the graph.
+        # Only nodes directly reachable from / adjacent to the initial vertex
+        # could ever be in the same clique.
+        # If an adjacent vertex is also adjacent to all other vertices in the
+        # intermediate clique, then it is in the clique.
         for p2 in data[p1]
             if intersect(group, data[p2]) == group
                 push!(group, p2)
@@ -121,7 +68,6 @@ function part2()
 
         push!(groups, group)
     end
-    groups = unique(groups)
 
     _, maxidx = findmax(length.(groups))
     pwd = sort([s for s in groups[maxidx]])
